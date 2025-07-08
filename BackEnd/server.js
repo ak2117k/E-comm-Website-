53bab647ade12f184bbac23e98e999a38afb3aea
Aryan Kumar <ak.2117k@gmail.com>
2025-07-08 12:00:30 +0530

    Deployed backend on render

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -13,7 +13,12 @@ const cartRoutes = require("./routes/cartRoutes");
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: " https://e-comm-website-gwb1.onrender.com",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(
   session({
